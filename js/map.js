1b8108a5d36d33cea6b7944d299c688fe6d957bd
Petr Pridal <petr.pridal@klokantech.com>
2015-05-30 13:12:51 +0300

    The Retro map from PastPlace MapBox account. Closes #4

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 /* MapBox map */
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiZGFsaWJvcmphbmFrIiwiYSI6IktJZnRiUEUifQ.S66RjNGVR8-J1wsY7f7Htg';
-var map = L.mapbox.map('map', 'mapbox.streets')
+L.mapbox.accessToken = 'pk.eyJ1IjoicGFzdHBsYWNlIiwiYSI6Inl1SkV3TzAifQ.2v6hn1Z-0hiYu_eDpjsP0g';
+var map = L.mapbox.map('map', 'pastplace.qr9c0udi')
         .setView([48, 21], 4);
 
 L.marker([48, 21], {
