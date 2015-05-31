45ad0534a200ee46071437d7df74daf8357d5213
Dalibor Janák <dalibor.janak@gmail.com>
2015-05-31 15:37:23 +0200

    map, logo, place

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 /* MapBox map */
 
 L.mapbox.accessToken = 'pk.eyJ1IjoicGFzdHBsYWNlIiwiYSI6Inl1SkV3TzAifQ.2v6hn1Z-0hiYu_eDpjsP0g';
-var map = L.mapbox.map('map', 'pastplace.qr9c0udi')
+var map = L.mapbox.map('map', 'pastplace.qr9c0udi', {zoomControl: false})
         .setView([48, 21], 4);
 
 L.marker([48, 21], {
@@ -14,4 +14,8 @@ L.marker([52, 0], {
   icon: L.mapbox.marker.icon({
     'marker-color': '#574f45'
   })
+}).addTo(map);
+
+L.control.zoom({
+  position: 'bottomright'
 }).addTo(map);
