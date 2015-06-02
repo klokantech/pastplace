8f8b68664a57709b7648c57d03ceea54a91e8f30
Dalibor Janák <dalibor.janak@gmail.com>
2015-06-02 21:29:00 +0200

    Markers for map

--- js/map.js
+++ js/map.js
@@ -4,17 +4,22 @@ L.mapbox.accessToken = 'pk.eyJ1IjoicGFzdHBsYWNlIiwiYSI6Inl1SkV3TzAifQ.2v6hn1Z-0h
 var map = L.mapbox.map('map', 'pastplace.qr9c0udi', {zoomControl: false})
         .setView([48, 21], 4);
 
-L.marker([48, 21], {
-  icon: L.mapbox.marker.icon({
-    'marker-color': '#574f45'
-  })
-}).addTo(map);
+var icon = L.icon({
+  iconUrl: '/img/marker.png',
+  iconSize: [44, 48],
+  iconAnchor: [18, 48]
+});
 
-L.marker([52, 0], {
-  icon: L.mapbox.marker.icon({
-    'marker-color': '#574f45'
-  })
-}).addTo(map);
+
+var iconActive = L.icon({
+  iconUrl: '/img/marker-blue.png',
+  iconSize: [44, 48],
+  iconAnchor: [18, 48]
+});
+
+L.marker([48, 21], {icon: icon}).addTo(map);
+L.marker([52, 0], {icon: icon}).addTo(map);
+L.marker([46.9446369, 7.4487306], {icon: iconActive}).addTo(map);
 
 L.control.zoom({
   position: 'bottomright'
